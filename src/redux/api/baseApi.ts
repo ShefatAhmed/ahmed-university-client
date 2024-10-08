fd046adcda8b7c5d11708b862ce2e494a3e7e5c3
unknown <shefata69@gmail.com>
2024-10-09 01:36:13 +0600

    fix error

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout } from "../features/auth/authSlice";
 
@@ -12,7 +12,7 @@ const baseQuery = fetchBaseQuery({
     }
 })
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> = async (args, api, extraOptions) : Promise<any> => {
     let result = await baseQuery(args, api, extraOptions)
     if (result.error?.status == 401) {
         const res = await fetch("http://localhost:3000/api/v1/auth/refresh-token", {
@@ -27,7 +27,7 @@ const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
                 token: data.data.accessToken
             })
             result = await baseQuery(args, api, extraOptions)
-        }else{
+        } else {
             api.dispatch(logout())
         }
     }
