ba13367f9d65e91405a938600af027379e1e1c69
unknown <shefata69@gmail.com>
2024-10-10 19:39:48 +0600

    complete academic semester

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,12 +1,31 @@
+import { TAcademicSemester, TQueryParam, TResponseRedux } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const academicMangementApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getAllSemesters: builder.query({
-            query: () => ({
-                url: "/academic-semesters",
-                method: "GET",
-            })
+            query: (args) => {
+                console.log(args);
+                const params = new URLSearchParams();
+
+                if (args) {
+                    args.forEach((item: TQueryParam) => {
+                        params.append(item.name, item.value as string);
+                    });
+                }
+
+                return {
+                    url: '/academic-semesters',
+                    method: 'GET',
+                    params: params,
+                };
+            },
+            transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+                return {
+                    data: response.data,
+                    meta: response.meta,
+                };
+            },
         }),
         addAcademicSemester: builder.mutation({
             query: (data) => ({
