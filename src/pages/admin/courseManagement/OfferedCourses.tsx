d14f1055c7ba6df333eca69284455d50366dc12d
unknown <shefata69@gmail.com>
2024-10-15 23:29:32 +0600

    start coursemanagement

---
+++ src/pages/admin/courseManagement/OfferedCourses.tsx
@@ -0,0 +1,9 @@
+const OfferedCourses = () => {
+  return (
+    <div>
+      <h1>This is OfferedCourses page</h1>
+    </div>
+  );
+};
+
+export default OfferedCourses;
---
+++ src/pages/admin/courseManagement/SemesterRegistration.tsx
@@ -0,0 +1,9 @@
+const SemesterRegistration = () => {
+  return (
+    <div>
+      <h1>This is SemesterRegistration page</h1>
+    </div>
+  );
+};
+
+export default SemesterRegistration;
