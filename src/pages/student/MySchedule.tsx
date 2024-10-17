dac1295e178225db48132cada00ce6c2e3022199
unknown <shefata69@gmail.com>
2024-10-17 16:54:57 +0600

    my schedule

--- src/pages/student/MySchedule.tsx
+++ src/pages/student/MySchedule.tsx
@@ -1,7 +1,22 @@
+import { useGetAllEnrolledCoursesQuery } from "../../redux/features/student/studentCourseApi";
+
 const MySchedule = () => {
+  const { data } = useGetAllEnrolledCoursesQuery(undefined);
   return (
     <div>
-      <h1>This is MySchedule page</h1>
+      {data?.data?.map((item:any) => {
+        return (
+          <div>
+            <div>{item.course.title}</div>
+            <div>{item.offeredCourse.section}</div>
+            <div>
+              {item.offeredCourse.days.map((item:any) => (
+                <span> {item}</span>
+              ))}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/routes/student.routes.tsx
+++ src/routes/student.routes.tsx
@@ -1,41 +1,20 @@
-import { NavLink } from "react-router-dom";
-import { TSiderbarItems } from "../types";
+import StudentDashboard from "../pages/student/StudentDashboard";
+import MySchedule from "../pages/student/MySchedule";
 import OfferedCourses from "../pages/student/OfferedCourses";
-
 export const studentPaths = [
     {
-        name: "Dashboard",
-        path: "dashboard",
-        element: <h1>Dashboard</h1>
+        name: 'Dashboard',
+        path: 'dashboard',
+        element: <StudentDashboard />,
     },
     {
-        name: "Offered Courses",
-        children: [
-            {
-                name: 'Offered Courses',
-                path: 'offered-courses',
-                element: <OfferedCourses />,
-            },
-        ]
-    }
+        name: 'Offered Course',
+        path: 'offered-course',
+        element: <OfferedCourses />,
+    },
+    {
+        name: 'My Schedule',
+        path: 'schedule',
+        element: <MySchedule />,
+    },
 ]
-
-export const studentSidberItems = studentPaths.reduce((acc: TSiderbarItems[], item) => {
-    if (item.path && item.element) {
-        acc.push({
-            key: item.name,
-            label: <NavLink to={`/student/${item.path}`}>{item.name}</NavLink>
-        });
-    }
-    if (item.children) {
-        acc.push({
-            key: item.name,
-            label: item.name,
-            children: item.children.map((child) => ({
-                key: child.name,
-                label: <NavLink to={`/student/${child.path}`}>{child.name}</NavLink>
-            }))
-        })
-    }
-    return acc
-}, [])
