277c0534fd0e725bf4898127db05b578a112a352
unknown <shefata69@gmail.com>
2024-10-17 09:21:49 +0600

    offer courses

--- src/pages/admin/courseManagement/OfferCourse.tsx
+++ src/pages/admin/courseManagement/OfferCourse.tsx
@@ -1,8 +1,123 @@
+import { Button, Col, Flex } from "antd";
+import AUForm from "../../../components/form/AUForm";
+import AUSelect from "../../../components/form/AUSelect";
+import AUInput from "../../../components/form/AUInput";
+import moment from "moment";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useCreateOfferedCourseMutation, useGetAllCoursesQuery, useGetAllRegisteredSemestersQuery, useGetCourseFacultiesQuery } from "../../../redux/features/admin/courseManagement";
+import { useGetAcademicDepartmentsQuery, useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { useState } from "react";
+import AUTimePicker from "../../../components/form/PHTimePicker";
+import AUSelectWithWatch from "../../../components/form/PHSelectWithWatch";
+import { weekDaysOptions } from "../../../constants/global";
+
 const OfferCourse = () => {
+  const [courseId, setCourseId] = useState('');
+
+  const [addOfferedCourse] = useCreateOfferedCourseMutation();
+
+  const { data: semesterRegistrationData } = useGetAllRegisteredSemestersQuery([
+    { name: 'sort', value: 'year' },
+    { name: 'status', value: 'UPCOMING' },
+  ]);
+
+  const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
+
+  const { data: academicDepartmentData } =
+    useGetAcademicDepartmentsQuery(undefined);
+
+  const { data: coursesData } = useGetAllCoursesQuery(undefined);
+
+  const { data: facultiesData, isFetching: fetchingFaculties } =
+    useGetCourseFacultiesQuery(courseId, { skip: !courseId });
+
+  const semesterRegistrationOptions = semesterRegistrationData?.data?.map(
+    (item) => ({
+      value: item._id,
+      label: `${item.academicSemester.name} ${item.academicSemester.year}`,
+    })
+  );
+
+  const academicFacultyOptions = academicFacultyData?.data?.map((item) => ({
+    value: item._id,
+    label: item.name,
+  }));
+
+  const academicDepartmentOptions = academicDepartmentData?.data?.map(
+    (item) => ({
+      value: item._id,
+      label: item.name,
+    })
+  );
+
+  const courseOptions = coursesData?.data?.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+
+  const facultiesOptions = facultiesData?.data?.faculties?.map((item: { _id: string; fullName: string; }) => ({
+    value: item._id,
+    label: item.fullName,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const offeredCourseData = {
+      ...data,
+      maxCapacity: Number(data.maxCapacity),
+      section: Number(data.section),
+      startTime: moment(new Date(data.startTime)).format('HH:mm'),
+      endTime: moment(new Date(data.endTime)).format('HH:mm'),
+    };
+
+    const res = await addOfferedCourse(offeredCourseData);
+    console.log(res);
+  };
   return (
-    <div>
-      <h1>This is OfferCourse page</h1>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <AUForm onSubmit={onSubmit}>
+          <AUSelect
+            name="semesterRegistration"
+            label="Semester Registrations"
+            options={semesterRegistrationOptions}
+          />
+          <AUSelect
+            name="academicFaculty"
+            label="Academic Faculty"
+            options={academicFacultyOptions}
+          />
+          <AUSelect
+            name="academicDepartment"
+            label="Academic Department"
+            options={academicDepartmentOptions}
+          />
+          <AUSelectWithWatch
+            onValueChange={setCourseId}
+            options={courseOptions}
+            name="course"
+            label="Course"
+          />
+          <AUSelect
+            disabled={!courseId || fetchingFaculties}
+            name="faculty"
+            label="Faculty"
+            options={facultiesOptions}
+          />
+          <AUInput type="text" name="section" label="Section" />
+          <AUInput type="text" name="maxCapacity" label="Max Capacity" />
+          <AUSelect
+            mode="multiple"
+            options={weekDaysOptions}
+            name="days"
+            label="Days"
+          />
+          <AUTimePicker name="startTime" label="Start Time" />
+          <AUTimePicker name="endTime" label="End Time" />
+
+          <Button htmlType="submit">Submit</Button>
+        </AUForm>
+      </Col>
+    </Flex>
   );
 };
 
--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,58 +1,75 @@
-import { TAcademicDepartment, TAcademicSemester, TQueryParam, TResponseRedux } from "../../../types";
+import { TAcademicDepartment, TAcademicFaculty, TAcademicSemester, TQueryParam, TResponseRedux } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const academicMangementApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllSemesters: builder.query({
-            query: (args) => {
-                console.log(args);
-                const params = new URLSearchParams();
+  endpoints: (builder) => ({
+    getAllSemesters: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
 
-                if (args) {
-                    args.forEach((item: TQueryParam) => {
-                        params.append(item.name, item.value as string);
-                    });
-                }
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
 
-                return {
-                    url: '/academic-semesters',
-                    method: 'GET',
-                    params: params,
-                };
-            },
-            transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
-                return {
-                    data: response.data,
-                    meta: response.meta,
-                };
-            },
-        }),
-        addAcademicSemester: builder.mutation({
-            query: (data) => ({
-                url: "/academic-semesters/create-academic-semester",
-                method: "POST",
-                body: data
-            })
-        }),
-        getAcademicDepartments: builder.query({
-            query: () => {
-              return { url: '/academic-departments', method: 'GET' };
-            },
-            transformResponse: (response: TResponseRedux<TAcademicDepartment[]>) => {
-              return {
-                data: response.data,
-                meta: response.meta,
-              };
-            },
-          }),
-          addAcademicDepartment: builder.mutation({
-            query: (data) => ({
-              url: '/academic-departments/create-academic-department',
-              method: 'POST',
-              body: data,
-            }),
-          }),
-    })
+        return {
+          url: '/academic-semesters',
+          method: 'GET',
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicSemester: builder.mutation({
+      query: (data) => ({
+        url: '/academic-semesters/create-academic-semester',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getAcademicFaculties: builder.query({
+      query: () => {
+        return { url: '/academic-faculties', method: 'GET' };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicFaculty[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicFaculty: builder.mutation({
+      query: (data) => ({
+        url: '/academic-faculties/create-academic-faculty',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getAcademicDepartments: builder.query({
+      query: () => {
+        return { url: '/academic-departments', method: 'GET' };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicDepartment[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicDepartment: builder.mutation({
+      query: (data) => ({
+        url: '/academic-departments/create-academic-department',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  })
 })
 
-export const { useGetAllSemestersQuery, useAddAcademicSemesterMutation, useGetAcademicDepartmentsQuery, useAddAcademicDepartmentMutation } = academicMangementApi
+export const { useGetAllSemestersQuery, useAddAcademicSemesterMutation, useGetAcademicDepartmentsQuery, useAddAcademicDepartmentMutation, useGetAcademicFacultiesQuery } = academicMangementApi
